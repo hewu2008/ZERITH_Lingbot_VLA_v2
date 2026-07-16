b663691dbeba8fe73a6440c002a965964caa2a7b
server <you@example.com>
2026-07-16 12:04:14 +0800

    feat: 添加零旦机器人训练相关的脚本与配置文件
    新增了三个文件：
    1.  数据集转换脚本convert_hdf5_to_lerobot.py，用于将Zerith H1 Pro的HDF5记录转换为LeRobot数据集格式
    2.  Zerith模型训练配置文件zerith.yaml
    3.  归一化统计量计算脚本run_zerith_compute_norm_stats.sh

---
+++ tools/run_zerith_compute_norm_stats.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DATASET_PATH="/home/jszn/hewu/dataset/hewu2008/pick_and_place_v2"
+
+CUDA_VISIBLE_DEVICES=0 bash train.sh scripts/compute_norm_stats.py ./configs/vla/norm_compute/post_data.yaml \
+  --data.data_name zerith \
+  --data.train_path "$DATASET_PATH" \
+  --data.robot_config_root ./configs/robot_configs \
+  --data.norm_path assets/norm_stats/zerith.json \
+  --data.data_ratio_for_norm_compute 1
