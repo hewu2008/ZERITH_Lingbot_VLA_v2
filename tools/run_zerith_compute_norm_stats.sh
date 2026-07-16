c8687e287f9426f4661a62b7cf3d847b2846e723
server <you@example.com>
2026-07-16 16:46:16 +0800

    build(scripts): adjust compute norm stats script parameters
    add num_workers 8 and change data ratio to 0.1 for norm stats calculation

--- tools/run_zerith_compute_norm_stats.sh
+++ tools/run_zerith_compute_norm_stats.sh
@@ -4,7 +4,8 @@ DATASET_PATH="/home/jszn/hewu/dataset/hewu2008/pick_and_place_lingbot_vla_v2"
 
 CUDA_VISIBLE_DEVICES=0 bash train.sh scripts/compute_norm_stats.py ./configs/vla/zerith/zerith.yaml \
   --data.data_name zerith \
+  --data.num_workers 8 \
   --data.train_path "$DATASET_PATH" \
   --data.robot_config_root ./configs/robot_configs \
   --data.norm_path assets/norm_stats/zerith.json \
-  --data.data_ratio_for_norm_compute 1
+  --data.data_ratio_for_norm_compute 0.1
