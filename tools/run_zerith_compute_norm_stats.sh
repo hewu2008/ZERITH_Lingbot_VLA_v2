32045c8af97a4a28227bde74116d053642709ac9
server <you@example.com>
2026-07-20 18:52:28 +0800

    chore(tools): 更新归一化统计量计算脚本的数据集路径和采样比例
    将数据集路径修改为1_clear_the_bin_box_20260720，同时将归一化计算的数据采样比例从0.25调整为0.5

--- tools/run_zerith_compute_norm_stats.sh
+++ tools/run_zerith_compute_norm_stats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATASET_PATH="/home/jszn/hewu/dataset/hewu2008/pick_and_place_lingbot_vla_v2"
+DATASET_PATH="/home/jszn/hewu/dataset/hewu2008/1_clear_the_bin_box_20260720"
 
 torchrun --nproc_per_node=1 scripts/compute_norm_stats.py ./configs/vla/zerith/zerith.yaml \
   --data.data_name zerith \
@@ -8,4 +8,4 @@ torchrun --nproc_per_node=1 scripts/compute_norm_stats.py ./configs/vla/zerith/z
   --data.train_path "$DATASET_PATH" \
   --data.robot_config_root ./configs/robot_configs \
   --data.norm_path assets/norm_stats/zerith.json \
-  --data.data_ratio_for_norm_compute 0.25
+  --data.data_ratio_for_norm_compute 0.5
