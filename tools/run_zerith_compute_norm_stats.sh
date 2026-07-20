a8903480c46d32e58ba9cdcb097debf244d7eb00
server <you@example.com>
2026-07-20 19:25:02 +0800

    fix(configs,tools: 调整数据增强和归一化计算参数
    1. 开启图像数据增强开关
    2. 将归一化计算的数据集比例从0.5改为1.0

--- tools/run_zerith_compute_norm_stats.sh
+++ tools/run_zerith_compute_norm_stats.sh
@@ -8,4 +8,4 @@ torchrun --nproc_per_node=1 scripts/compute_norm_stats.py ./configs/vla/zerith/z
   --data.train_path "$DATASET_PATH" \
   --data.robot_config_root ./configs/robot_configs \
   --data.norm_path assets/norm_stats/zerith.json \
-  --data.data_ratio_for_norm_compute 0.5
+  --data.data_ratio_for_norm_compute 1.0
