4d9fa13079da4f019feba372141d8012bf8e370d
server <you@example.com>
2026-07-21 09:49:16 +0800

    chore(scripts): 更新hdf5转lerobot脚本的数据集路径和时间戳
    将数据集路径、仓库ID从20260720更新为20260721，适配最新的采集数据

--- tools/run_convert_hdf5_to_lerobot.sh
+++ tools/run_convert_hdf5_to_lerobot.sh
@@ -3,6 +3,6 @@
 export HF_LEROBOT_HOME=/home/jszn/hewu/dataset/
 
 python scripts/convert_hdf5_to_lerobot.py \
-  --raw_dir /home/jszn/hewu/dataset/hdf5/1_clear_the_bin_box_20260720 \
-  --repo_id hewu2008/1_clear_the_bin_box_20260720 \
+  --raw_dir /home/jszn/hewu/dataset/hdf5/1_clear_the_bin_box_20260721 \
+  --repo_id hewu2008/1_clear_the_bin_box_20260721 \
   --task 'clear the bin box'
