1b15d4996ccde9763cb39ed459c1a1d8cb4e7323
hewu <hewu2008@gmail.com>
2026-07-22 17:02:11 +0800

    chore(tools): 修正下载脚本使用的脚本文件名
    将原调用的download_ms_model.sh改为download_hf_model.sh，适配hf模型下载需求

--- tools/run_download_hf_model.sh
+++ tools/run_download_hf_model.sh
@@ -10,6 +10,6 @@ python scripts/download_ms_model.py \
     --repo_id Qwen/Qwen3-VL-4B-Instruct \
     --local_dir /data/4T-1/hewu/model_zoo
 
-python scripts/download_ms_model.py \
+python scripts/download_hf_model.py \
     --repo_id Ruicheng/moge-2-vitb-normal \
     --local_dir /data/4T-1/hewu/model_zoo
