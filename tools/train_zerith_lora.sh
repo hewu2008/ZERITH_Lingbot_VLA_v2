db74ca4b3e4d3361a7e517d1844ab1d495cc8330
hewu <hewu2008@gmail.com>
2026-07-22 20:01:05 +0800

    chore(train script): adjust learning rate to 4e-4
    将训练脚本中的学习率从1e-4修改为4e-4

--- tools/train_zerith_lora.sh
+++ tools/train_zerith_lora.sh
@@ -33,6 +33,6 @@ python -m torch.distributed.run \
   --train.lora_rank=64 \
   --train.lora_alpha=64 \
   --train.freeze_non_lora=true \
-  --train.lr=1e-4 \
+  --train.lr=4e-4 \
   --train.output_dir output/zerith_lora \
   2>&1 | tee output/log_zerith_lora.txt
