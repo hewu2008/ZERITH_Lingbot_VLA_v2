d306f5ccf30f63b65f91a4fdf1d80961bfcd5190
hewu <hewu2008@gmail.com>
2026-07-22 16:48:49 +0800

    refactor: 更新训练脚本与配置的路径与超参数
    1.  统一修改所有文件中的本地路径为/data/4T-1/hewu/下的实际路径
    2.  调整LoRA秩和alpha为64，更新训练batch相关配置
    3.  修改base.position的归一化方式为meanstd

--- tools/run_download_hf_model.sh
+++ tools/run_download_hf_model.sh
@@ -4,12 +4,12 @@ export HF_ENDPOINT=https://hf-mirror.com
 
 python scripts/download_ms_model.py \
     --repo_id robbyant/lingbot-vla-v2-6b \
-    --local_dir /mnt/d/wsl/model_zoo
+    --local_dir /data/4T-1/hewu/model_zoo
 
 python scripts/download_ms_model.py \
     --repo_id Qwen/Qwen3-VL-4B-Instruct \
-    --local_dir /mnt/d/wsl/model_zoo
+    --local_dir /data/4T-1/hewu/model_zoo
 
 python scripts/download_ms_model.py \
     --repo_id Ruicheng/moge-2-vitb-normal \
-    --local_dir /mnt/d/wsl/model_zoo
+    --local_dir /data/4T-1/hewu/model_zoo
